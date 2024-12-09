aa93feb5546e6618e6fce546ff77af83d182aef1
alex.js <zhongwcool@163.com>
2024-12-09 10:04:59 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ProjectPaper
 Pick wallpaper from windows' spotlight
+
+### Overview
+
+> [!NOTE]
+> 工程使用PyCharm开发，Python 3.12+，主要用于提取Windows的Spotlight壁纸。
+
+### 运行条件
+
+#### 依赖库
+
+无
+
+### 打包
+
+使用**pyinstaller**打包成exe文件，打开**PyCharm**的`Terminal`输入：
+
+```shell
+pyinstaller --onefile --name PaperPicker --icon app.ico main.py
+```
+
+如果需要在exe文件名中添加版本号、版权、公司等版本信息，可以使用`--version-file`参数，例如：
+
+```shell
+pyinstaller --onefile --version-file version.txt --name PaperPicker --icon app.ico main.py
+```
+
+### 其他
+
+#### 1.如何更新pip
+
+```shell
+python -m pip install --upgrade pip
+```
+
+![](https://raw.githubusercontent.com/zhongwcool/ProjectPaper/main/Assets/app-logo.png)
---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+from pathlib import Path
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    # 获取当前用户的图片文件夹路径
+    pictures_dir = Path.home() / "Desktop"
+
+    # Windows Spotlight 图片所在的目录
+    spotlight_path = os.path.join(os.getenv('LOCALAPPDATA'),
+                                  'Packages',
+                                  'Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy',
+                                  'LocalState', 'Assets')
+
+    # 导出 Spotlight 图片的目标文件夹
+    export_folder = pictures_dir / "Spotlight"
+    export_folder.mkdir(exist_ok=True)
+
+    # 复制并重命名图片
+    for filename in os.listdir(spotlight_path):
+        # 构建完整的文件路径
+        source_file = os.path.join(spotlight_path, filename)
+
+        # 简单的过滤，通过文件大小过滤非图片内容（这里设定图片至少为 100KB）
+        if os.path.getsize(source_file) > 100 * 1024:
+            target_file = export_folder / (filename + ".jpg")
+
+            # 如果存在命名冲突，不重复复制
+            if not target_file.exists():
+                shutil.copy2(source_file, target_file)
+
+    print(f"Spotlight images have been exported to: {export_folder}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    user_name = os.getlogin()
+    print_hi(user_name)
+    input("按任意键退出...")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
